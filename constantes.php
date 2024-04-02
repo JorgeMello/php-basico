78eb869fc9907fb1aff21e7379504331273cb9bd
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-02 18:51:42 -0300

    Constantes sintaxe

---
+++ constantes.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo "CONSTANTES : SINTAXES E OPERADORES <br>";
+echo 'define("PI", 3.14); <br>';
+
+define("PI", 3.14);
+echo PI;
+echo "<br>";
+echo "PI * 2 =<br>";
+echo PI * 2;
+echo "<br>";
+echo "<br>";
+echo 'define("NOME", "JORGE"); <br>';
+define("NOME", "JORGE");
+echo NOME;
+echo "<br>";
+echo 'const HOST = "localhost"; <br>';
+const HOST = "localhost";
+echo HOST;
