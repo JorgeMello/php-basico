8e79ee3b26fc2c1b60952361d919619c2b6a3325
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-02 18:40:08 -0300

    Váriáveis e tipos de dados

---
+++ tipos_de_dados.php
@@ -0,0 +1,30 @@
+<?php 
+echo "Tipos de dados em PHP";
+echo "<br>";
+echo "<br>";
+echo "BOLEANO: TRUE (1) ou FALSE (0)";
+echo "<br>";
+echo "<br>";
+echo "VARIAVEIS STRING, string (texto)";
+echo "<br>";
+
+$nome = "Joaquim";
+echo $nome;
+echo "<br>";
+$idade = 19;
+echo $idade;
+echo "<br>";
+$altura = 1.77;
+echo $altura;
+echo "<br>";
+$casado = true;
+echo $casado;
+echo "<br>";
+$filhos = null;
+echo $filhos;
+echo "<br>";
+
+
+
+
+
