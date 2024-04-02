a7006a124c3e892301b037eb9227a30193c4a43a
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-02 18:28:00 -0300

    PHP Básico - COMENTÁRIOS (TAGS)

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo "Comentário em PHP 8 <br>"; 
+echo "<br>";
+echo "Comentar uma única linha:";
+echo "<br>";
+echo "<br>";
+echo "//";
+echo "<br>";
+echo "ou";
+echo "<br>";
+echo "#";
+echo "<br>";
+echo "<br>";
+echo "Comentar  várias linhas:";
+echo "<br>";
+echo "<br>";
+echo '"<!-- texto a ser comentado -->"';
+echo "<br>";
+echo "ou ";
+echo "/* texto a ser comentado */";
+
+
+
