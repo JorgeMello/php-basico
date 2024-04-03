e784891c20ca95b63fd5e69b594a875f6c74054b
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-02 23:55:04 -0300

    Mostrar e Atribuir valores para variáveis

---
+++ mostrar_valor_variavel.php
@@ -0,0 +1,23 @@
+<?php 
+
+echo "VARIAVEIS STRING, string (texto)";
+echo "<br>";
+echo '$nome = "Joaquim";';
+echo "<br>";
+echo 'echo $nome;';
+echo "<br>";
+$nome = "Joaquim";
+echo $nome;
+echo "<br>";
+$idade = 19;
+echo $idade;
+echo "<br>";
+$altura = 1.77;
+echo $altura;
+echo "<br>";
+$casado = true;
+echo $casado;
+echo "<br>";
+$filhos = null;
+echo $filhos;
+echo "<br>";
