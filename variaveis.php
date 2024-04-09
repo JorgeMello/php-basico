96576aa37ac49e01129cc412d6d44b3fb4ab9085
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-08 21:59:32 -0300

    variáveis atulização

--- variaveis.php
+++ variaveis.php
@@ -6,4 +6,11 @@ echo "$var, $Var";      // exibe "Bob, Joe" as variáveis são case-sensitive
 // $4site = 'not yet';     // inválido; começa com um número
 $_4site = 'not yet';    // válido; começa com um sublinhado
 $täyte = 'mansikka';    // válido; 'ä' é um caracter ASCII (extendido) 228
-?>
+
+
+$name = "Guilherme";
+$age = 20;
+
+echo $name; // Guilherme
+echo "</br>";
+echo $age; // 20
