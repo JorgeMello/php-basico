bd73ded927dcca869d7dacd92fc63dc58912ae16
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-08 22:03:58 -0300

    array multidimensional

--- array-atribuicao.php
+++ array-atribuicao.php
@@ -10,4 +10,11 @@ $item['unicorn'] = '';
 $item['unicorn'] = 'Pink unicorn';
 //isset($item['unicorn']) is still true. And empty($item) is still false. 
 //But now empty($item['unicorn']) is false;
-?>
+
+
+$php = array("Zend" => "CERTIFICAÇÃO", 6 => false);
+echo $php["Zend"]; // CERTIFICAÇÃO
+echo $php[6]; // 0
+
+// Zend é nossa chave e CERTIFICAÇÃO nosso valor
+// 6 é nossa chave e false(0) é nosso valor
