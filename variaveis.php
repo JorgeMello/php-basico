abbe684ac547ff404ede5ea668e1eb27e844d481
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-08 21:43:35 -0300

    Variáveis

---
+++ variaveis.php
@@ -0,0 +1,9 @@
+<?php
+$var = 'João';
+$Var = 'José';
+echo "$var, $Var";      // exibe "Bob, Joe"
+
+$4site = 'not yet';     // inválido; começa com um número
+$_4site = 'not yet';    // válido; começa com um sublinhado
+$täyte = 'mansikka';    // válido; 'ä' é um caracter ASCII (extendido) 228
+?>
