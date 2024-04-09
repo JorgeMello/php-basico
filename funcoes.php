e8d4b3abb3eae65544888bb870a5634a66c14942
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-08 21:47:42 -0300

    funções

--- funcoes.php
+++ funcoes.php
@@ -1,12 +1,13 @@
 <?php
 $foo = 25;
 $bar = &$foo;      // Esta atribuição é válida.
-$bar = &(24 * 7);  // Inválido; referencia uma expressão sem nome.
+// $bar = &(24 * 7);  // Inválido; referencia uma expressão sem nome.
 
 function test()
 {
    return 25;
 }
 
-$bar = &test();    // Inválido.
+echo $foo
+// $bar = &test();    // Inválido.
 ?>
